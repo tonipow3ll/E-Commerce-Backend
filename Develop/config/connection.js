cb557af5ba7eeaeb3086dc27414e40a8f8c508fd
Toni Powell <tonipow3ll@gmail.com>
2021-03-13 13:37:49 -0500

    initial commit

--- Develop/config/connection.js
+++ Develop/config/connection.js
@@ -2,8 +2,8 @@ require('dotenv').config();
 
 const Sequelize = require('sequelize');
 
-const sequelize = process.env.JAWSDB_URL
-  ? new Sequelize(process.env.JAWSDB_URL)
+const sequelize = process.env.ecommerce_db
+  ? new Sequelize(process.env.ecommerce_db)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
       host: 'localhost',
       dialect: 'mysql',
